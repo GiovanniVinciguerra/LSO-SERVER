79b3bc04b54799c6c2196933733748da56102850
GiovanniVinciguerra <giovy.vinci@gmail.com>
2025-02-19 22:32:54 +0100

    Prova fix get_user in signin.c

--- signin.c
+++ signin.c
@@ -4,8 +4,7 @@ struct user* get_user(char* buffer_pt) {
     struct user* new_user = (struct user*)malloc(sizeof(struct user));
     char* temp_buff = (char*)malloc(sizeof(char) * TEMP_BUFFER_SIZE);
     int copy_check = 0, byte_copy = 0, choice = 0;
-
-
+    printf("Sono qui\n");
     while(*buffer_pt != '}') {
         if(*buffer_pt == ':' && *(buffer_pt + 1) == '"') {
             copy_check = 1;
